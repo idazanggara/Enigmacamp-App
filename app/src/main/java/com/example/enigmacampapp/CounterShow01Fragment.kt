0362db45b073c178088f87792e5efcd6821091f3
idazanggara <idazideas@gmail.com>
2024-10-22 16:10:21 +0700

    Hands-on pertama fragment, counterApp fix sebelum di ubah menggunakan interface

--- app/src/main/java/com/example/enigmacampapp/CounterShow01Fragment.kt
+++ app/src/main/java/com/example/enigmacampapp/CounterShow01Fragment.kt
@@ -1,10 +1,13 @@
 package com.example.enigmacampapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.enigmacampapp.databinding.FragmentCounterShow01Binding
 
 //// TODO: Rename parameter arguments, choose names that match
 //// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +16,16 @@ import android.view.ViewGroup
 //
 ///**
 // * A simple [Fragment] subclass.
-// * Use the [ShowCounter01Fragment.newInstance] factory method to
+// * Use the [CounterShow01Fragment.newInstance] factory method to
 // * create an instance of this fragment.
 // */
-class ShowCounter01Fragment : Fragment() {
+class CounterShow01Fragment : Fragment() {
 //    // TODO: Rename and change types of parameters
 //    private var param1: String? = null
 //    private var param2: String? = null
+
+    // view binding
+    lateinit var binding: FragmentCounterShow01Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +40,28 @@ class ShowCounter01Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_counter01, container, false)
+//        return inflater.inflate(R.layout.fragment_counter_show01, container, false)
+        binding = FragmentCounterShow01Binding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun notifyShowCounter(counter: Int) {
+        binding.apply {
+            tvCounter.text = counter.toString()
+            if (counter > 10){
+                tvCounter.setTextColor(Color.GREEN)
+            } else if (counter < 0){
+                tvCounter.setTextColor(Color.RED)
+            } else {
+                tvCounter.setTextColor(Color.BLACK)
+            }
+        }
+
+        Log.i("testShowFragment", "showCounter $counter")
     }
 
     companion object {
@@ -44,12 +71,12 @@ class ShowCounter01Fragment : Fragment() {
 //         *
 //         * @param param1 Parameter 1.
 //         * @param param2 Parameter 2.
-//         * @return A new instance of fragment ShowCounter01Fragment.
+//         * @return A new instance of fragment CounterShow01Fragment.
 //         */
 //        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) = ShowCounter01Fragment()
-//            ShowCounter01Fragment().apply {
+        fun newInstance(param1: String, param2: String) = CounterShow01Fragment()
+//            CounterShow01Fragment().apply {
 //                arguments = Bundle().apply {
 //                    putString(ARG_PARAM1, param1)
 //                    putString(ARG_PARAM2, param2)
